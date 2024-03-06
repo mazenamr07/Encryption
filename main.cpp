34ae48e45373a96a435593f5520324d41d918171
Mazen <11410120230308@stud.cu.edu.eg>
2024-03-07 00:07:22 +0200

    Menu Done

--- main.cpp
+++ main.cpp
@@ -1,6 +1,51 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World! and modhek" << std::endl;
-    return 0;
+    // menu start and choice
+    cout << "Ahlan ya user ya habibi.\n"
+            "What would you like to do today?\n";
+
+    while (true) {
+        string choice_1;
+        string choice_2;
+
+        cout << "1- Cipher a message\n"
+                "2- Decipher a message\n"
+                "3- End\n" << ">>";
+
+        getline(cin, choice_1);
+        // checking if input is valid
+        while (choice_1 != "1" and choice_1 != "2" and choice_1 != "3") {
+            cout << "Please select a valid option:\n" << ">>";
+            getline(cin, choice_1);
+        }
+
+        if (choice_1 == "3") {
+            cout << "Bye bye!";
+            return 0;
+        }
+
+        cout << "\nWhich Cipher would you like to choose?" << endl;
+        cout << "1- First cypher\n"
+                "2- Second cypher\n"
+                "3- Third cypher\n"
+                "4- Return\n" << ">>";
+
+        getline(cin, choice_2);
+        // checking if input is valid
+        while (choice_2 != "1" and choice_2 != "2" and choice_2 != "3" and choice_2 != "4") {
+            cout << "Please select a valid option:\n" << ">>";
+            getline(cin, choice_2);
+        }
+
+        if (choice_2 == "4") {
+            cout << "Choose again:" << endl;
+            continue;
+        }
+
+        cout << choice_1 << "and" << choice_2;
+    }
 }
